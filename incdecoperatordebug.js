e31ddcfe800379a8b37f237725b2696c138e9994
Ranjkanna <90195660+Ranjkanna@users.noreply.github.com>
2026-05-23 09:29:37 +0530

    Add files via upload

---
+++ incdecoperatordebug.js
@@ -0,0 +1,20 @@
+let counter = 10;
+let updatedCounter=++counter;
+
+console.log(`Counter is currently ${updatedCounter}`);
+let score=8;
+
+let finalScore = score++;
+console.log(`Score is still ${finalScore}`);
+
+let coins = 3;
+console.log(`Coins is currently ${coins}`);
+
+let updatedCoins = --coins;
+console.log(`Coins is now ${updatedCoins}`);
+
+let health = 7;
+console.log(`Health is currently ${health}`);
+
+let newHealth = health--;
+console.log(`Health is still ${newHealth}`);
